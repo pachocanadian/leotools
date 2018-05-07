092da0575162953f91ab176d943b63ed00eb9d3c
Mike Carr <mcarr@pachogrande.com>
2018-05-06 23:02:09 -0700

    Added an index page so I can pull this into my web hosting

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
+       Remove this if you use the .htaccess -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>leotools</title>
+  <meta name="description" content="">
+  <meta name="author" content="Mike">
+
+  <meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+</head>
+
+<body>
+  <div>
+    <header>
+      <h1>leotools</h1>
+    </header>
+    <nav>
+      <p><a href="/">Home</a></p>
+      <p><a href="https://github.com/pachocanadian/leotools/">github</a></p>
+    </nav>
+
+    <div>
+		<a href="samples/pull-current-district-dcp.html">samples/pull-current-district-dcp.html</a>      
+    </div>
+
+  </div>
+</body>
+</html>
