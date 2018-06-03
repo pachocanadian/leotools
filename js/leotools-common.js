d33aef716f84c2c777b142842ae36e49959fa972
Mike Carr <mcarr@pachogrande.com>
2018-06-02 21:03:36 -0700

    Added district summary report

--- js/leotools-common.js
+++ js/leotools-common.js
@@ -57,3 +57,20 @@ function lt_current_division_performance_url(district)
 	var report_csv_url = 'http://dashboards.toastmasters.org/export.aspx?type=CSV&report=divisionperformance~' + district + '~' + date_string + '~~' + tm_year;	
 	return report_csv_url;
 }
+
+/**
+ * Returns the current district summary URL for the dashboard based on today's date
+ */
+function lt_current_district_summary_url()
+{
+	// date_string should be populated with a date in MM/DD/YYYY format corresponding to the last day of the current month	
+	today = new Date();	
+	endOfMonth = new Date(today.getFullYear(), today.getMonth()+1, 0);	
+	date_string = (endOfMonth.getMonth()+1) + '/' + endOfMonth.getDate() + '/' + endOfMonth.getFullYear();
+	
+	// TOFIX: This is hardcoded and will break. I'm not sure how to handle edge cases after June 30th at present
+	tm_year = '2017-2018';			
+			
+	var report_csv_url = 'http://dashboards.toastmasters.org/export.aspx?type=CSV&report=districtsummary~' + date_string + '~~' + tm_year;	
+	return report_csv_url;
+}
