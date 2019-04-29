b1071c46fd1e544cbba54ed6b35e6eac8e0b11d9
Mike Carr <mcarr@pachogrande.com>
2019-04-29 11:33:35 -0700

    new district-comparison which will unify district historical stats revision to leotools-common to relect 2018-2019 year

--- js/leotools-common.js
+++ js/leotools-common.js
@@ -5,7 +5,7 @@ function Leotools(args)
 	
 	this.cors_proxy = ( typeof args.cors_proxy !== 'undefined') ? args.cors_proxy : 'https://code.pachogrande.com/leotools/cors/proxy.php/';
 	this.district = ( typeof args.district !== 'undefined') ? args.district : '96';
-	this.tm_year = ( typeof args.tm_year !== 'undefined') ? args.tm_year : '2017-2018';
+	this.tm_year = ( typeof args.tm_year !== 'undefined') ? args.tm_year : '2018-2019';
 	
 	this.current_club_performance_url = function()
 		{
