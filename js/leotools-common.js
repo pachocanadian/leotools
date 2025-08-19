dcd4be8fc66a2ead3892cfefc9c9b19aec3caec9
Mike Carr <mike.carr@wintonhomes.ca>
2025-08-18 17:07:52 -0700

    revised url to new standard

--- js/leotools-common.js
+++ js/leotools-common.js
@@ -3,9 +3,10 @@ function Leotools(args)
 {
 	args = (typeof args !== 'undefined') ? args : {};
 	
+	// https://dashboards.toastmasters.org/export.aspx?type=CSV&report=districtperformance~21~8/31/2025~~2025-2026
 	this.cors_proxy = ( typeof args.cors_proxy !== 'undefined') ? args.cors_proxy : 'https://code.pachogrande.com/leotools/cors/proxy.php/';
-	this.district = ( typeof args.district !== 'undefined') ? args.district : '96';
-	this.tm_year = ( typeof args.tm_year !== 'undefined') ? args.tm_year : '2024-2025';
+	this.district = ( typeof args.district !== 'undefined') ? args.district : '21';
+	this.tm_year = ( typeof args.tm_year !== 'undefined') ? args.tm_year : '2025-2026';
 	
 	this.current_club_performance_url = function()
 		{
